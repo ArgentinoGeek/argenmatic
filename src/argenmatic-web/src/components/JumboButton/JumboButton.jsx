51fa0193658841454e3b8eadcb95be218c1aaef1
Nicolás Ruiz Neiman <nicolasruizneiman@gmail.com>
2021-06-04 19:21:22 -0300

    Secondary button texts fixed

--- src/argenmatic-web/src/components/JumboButton/JumboButton.jsx
+++ src/argenmatic-web/src/components/JumboButton/JumboButton.jsx
@@ -3,8 +3,8 @@ import styles from './JumboButton.module.css';
 const JumboButton = ({destinationUrl, primaryText, secondaryText, isPrimary}) => {
    return (
         <a href={destinationUrl} className={isPrimary ? styles.button : styles.buttonSecondary}>
-            <span className={styles.primaryText}>{primaryText}</span>
-            <span className={styles.secondaryText}>{secondaryText}</span>
+            <span className={isPrimary ? styles.primaryText : styles.primaryTextAlternated}>{primaryText}</span>
+            <span className={isPrimary ? styles.secondaryText : styles.secondaryTextAlternated}>{secondaryText}</span>
         </a>
     );
 }
