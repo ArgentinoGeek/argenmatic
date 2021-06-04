74814cff63eb45373cf71b2d88117d8d635ec891
Nicolás Ruiz Neiman <nicolasruizneiman@gmail.com>
2021-06-04 20:41:43 -0300

    Header component implementation

--- src/argenmatic-web/src/App.jsx
+++ src/argenmatic-web/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import FancyNavbar from './components/FancyNavbar/FancyNavbar';
+import Header from './components/Header/Header';
 import JumboButton from './components/JumboButton/JumboButton';
 
 function App() {
   return (
     <div className="App">
       <FancyNavbar />
+      <Header resumesCreated={120587452} />
       <JumboButton
       primaryText="Create a resume"
       secondaryText="For free"
---
+++ src/argenmatic-web/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css';
+import background from '../../img/papers.jpg';
+
+const Header = ({resumesCreated}) => {
+    return (
+        <div className={styles.header}>
+            <img src={background} alt="Backgound papers" />
+            <div className={styles.content}>
+                <span className={styles.resumesCount}>{resumesCreated.toLocaleString()}</span>
+                <span className={styles.label}>Resumes created</span>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
