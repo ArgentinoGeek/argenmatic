131923401fbd3b032e7af331a358e23273ef1308
Nicolás Ruiz Neiman <nicolasruizneiman@gmail.com>
2021-06-04 21:08:13 -0300

    Cards implementation

--- src/argenmatic-web/src/App.jsx
+++ src/argenmatic-web/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import FancyNavbar from './components/FancyNavbar/FancyNavbar';
 import JumboButton from './components/JumboButton/JumboButton';
+import Card from './components/Card/Card';
 
 function App() {
   return (
@@ -18,6 +19,18 @@ function App() {
       destinationUrl="https://github.com/ArgentinoGeek/argenmatic"
       isPrimary={true}
        />
+       <Card title="About">
+        {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices ut sem vel vestibulum. Mauris vel lacus et sapien egestas faucibus. In eu magna turpis. Donec in nunc a mauris lacinia egestas sit amet id velit. Nulla et quam sit amet augue feugiat hendrerit. Vestibulum gravida et felis nec interdum. Suspendisse potenti. Phasellus a purus ornare, placerat erat fermentum, efficitur eros. Fusce molestie magna nulla, sed eleifend erat euismod id. Vestibulum non gravida eros. Donec interdum pellentesque tellus eu mattis. Vestibulum posuere tristique cursus."}
+       </Card>
+
+       <Card title="Pricing">
+          <JumboButton
+             primaryText="Log in"
+             secondaryText="to save your information"
+             destinationUrl="https://github.com/ArgentinoGeek/argenmatic"
+             isPrimary={true}
+          />
+       </Card>
     </div>
   );
 }
---
+++ src/argenmatic-web/src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import styles from './Card.module.css';
+
+const Card = (props) => {
+    return(
+        <div className={styles.card}>
+            <span className={styles.title}>{props.title}</span>
+            <span className={styles.content}>{props.children}</span>
+        </div>
+    );
+}
+
+export default Card;
