d904a8814772115c55897c296ee76435ff3d2eec
Nicolás Ruiz Neiman <nicolasruizneiman@gmail.com>
2021-06-09 21:22:41 -0300

    Fixing home page layout

--- src/argenmatic-web/src/pages/HomePage/HomePage.jsx
+++ src/argenmatic-web/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-// eslint-disable-next-line
 import styles from './HomePage.module.css';
 import FancyNavbar from '../../components/FancyNavbar/FancyNavbar';
 import Header from '../../components/Header/Header';
@@ -15,30 +14,38 @@ const HomePage = () => {
         <div>
             <FancyNavbar />
             <Header resumesCreated={120587452} />
-            <JumboButton
-                primaryText="Create a resume"
-                secondaryText="For free"
-                destinationUrl="/"
-                isPrimary={false}
-            />
-            <JumboButton
-                primaryText="Log in"
-                secondaryText="to save your information"
-                destinationUrl="/login"
-                isPrimary={true}
-            />
-            <Card title="About">
-                {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices ut sem vel vestibulum. Mauris vel lacus et sapien egestas faucibus. In eu magna turpis. Donec in nunc a mauris lacinia egestas sit amet id velit. Nulla et quam sit amet augue feugiat hendrerit. Vestibulum gravida et felis nec interdum. Suspendisse potenti. Phasellus a purus ornare, placerat erat fermentum, efficitur eros. Fusce molestie magna nulla, sed eleifend erat euismod id. Vestibulum non gravida eros. Donec interdum pellentesque tellus eu mattis. Vestibulum posuere tristique cursus."}
-            </Card>
-
-            <Card title="Pricing">
-                <JumboButton
-                    primaryText="Log in"
-                    secondaryText="to save your information"
-                    destinationUrl="/login"
-                    isPrimary={true}
-                />
-            </Card>
+            <div className={styles.bodyContainer}>
+                <div className={styles.buttonContainer}>
+                    <div className={styles.button}>
+                        <JumboButton
+                            primaryText="Create a resume"
+                            secondaryText="For free"
+                            destinationUrl="/"
+                            isPrimary={false}
+                        />
+                    </div>
+                    <div className={styles.button}>
+                        <JumboButton
+                            primaryText="Log in"
+                            secondaryText="to save your information"
+                            destinationUrl="/login"
+                            isPrimary={true}
+                        />
+                    </div>
+                </div>
+                <Card title="About">
+                    {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices ut sem vel vestibulum. Mauris vel lacus et sapien egestas faucibus. In eu magna turpis. Donec in nunc a mauris lacinia egestas sit amet id velit. Nulla et quam sit amet augue feugiat hendrerit. Vestibulum gravida et felis nec interdum. Suspendisse potenti. Phasellus a purus ornare, placerat erat fermentum, efficitur eros. Fusce molestie magna nulla, sed eleifend erat euismod id. Vestibulum non gravida eros. Donec interdum pellentesque tellus eu mattis. Vestibulum posuere tristique cursus."}
+                </Card>
+        
+                <Card title="Pricing">
+                    <JumboButton
+                        primaryText="Log in"
+                        secondaryText="to save your information"
+                        destinationUrl="/login"
+                        isPrimary={true}
+                    />
+                </Card>
+            </div>
         </div>
     );
 }
